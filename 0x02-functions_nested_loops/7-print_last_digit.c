6723fe27dd308d50f6e559ea51351370df85f7ab
Rick-Reign <rickreign6740@gamil.com>
2022-12-14 15:11:01 -0800

    Prints the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,15 +5,13 @@
  * @n: the number
  * Return: value of the last digit
  */
-int print_last_digit(int r)
+int print_last_digit(int n)
 {
-	int r;
+	int ld = n % 10;
 
-	print_last_digit(98);
-	print_last_digit(0);
-	r = print_last_digit(-1024);
-	_putchar('0' + r);
-	_putchar('\n');
+	if (ld < 0)
+		ld = -1;
+	_putchar(ld + '0');
 
 	return (0);
 }
