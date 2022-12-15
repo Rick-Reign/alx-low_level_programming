2dea899fdba4c266d1c32a64ff16679e0e9367b6
Rick-Reign <rickreign6740@gamil.com>
2022-12-15 10:32:24 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * mul - multiplies two integers.
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
