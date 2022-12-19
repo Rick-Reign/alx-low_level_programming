b5113c191879d1529aba159092d22082b17de53f
Rick-Reign <rickreign6740@gamil.com>
2022-12-19 11:47:36 -0800

    Prints string in putchar

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts -write a functon that prints a string, followed by a new line, to
+ * stdout.
+ *
+ * @str: This is my input string
+ */
+
+void _puts(char *str)
+{
+	int index;
+
+	for (index = 0; stri[index] != '\0'; index)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
