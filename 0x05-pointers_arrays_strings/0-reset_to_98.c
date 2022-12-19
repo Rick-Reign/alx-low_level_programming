1db5a2aa86736db92b1e6690bdd57fad82052422
Rick-Reign <rickreign6740@gamil.com>
2022-12-19 11:00:39 -0800

    Task Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_t0_98 - function that takes a pointer to an int parameter and
+ * updates the value it points to 98
+ *
+ * @n: This is the entry
+ */
+
+void reset_t0_98(int *n)
+{
+	*n = 98;
+}
