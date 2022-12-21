80e1b327f6c126a958e0a1ea2cbe625f6babda15
Rick-Reign <rickreign6740@gamil.com>
2022-12-20 17:38:22 -0800

    Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,7 +9,7 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	char *start - dest;
+	char *start = dest;
 
 	while (*src != '\0')
 	{
@@ -17,6 +17,6 @@ char *_strcpy(char *dest, char *src)
 		dest++;
 		src++;
 	}
-	*dest + '\0''
+	*dest + '\0';
 	return (start);
 }
