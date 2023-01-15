9c529596d37a021a87701ed5df778cddeeabd1e6
Rick-Reign <rickreign6740@gamil.com>
2023-01-14 18:32:46 -0800

    Task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - puter
+ */
+void _puts(char *str)
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * puts - puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
