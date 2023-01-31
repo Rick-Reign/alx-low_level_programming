bc92243c98a28951f3e7f27768bb81ffd9cb4132
Rick-Reign <rickreign6740@gamil.com>
2023-01-30 17:13:53 -0800

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
-	
+
 	while (h)
 	{
 		if (h->str == NULL)
@@ -20,6 +20,6 @@ size_t print_list(const list_t *h)
 		nodes++;
 		h = h->next;
 	}
-	
+
 	return (s);
 }
